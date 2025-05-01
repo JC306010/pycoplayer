8972178d9fabd38510b86ac74562b3d35f947205
310464 <156846632+310464@users.noreply.github.com>
2025-05-01 12:10:09 -0500

    Update to test.py

--- test.py
+++ test.py
@@ -6,4 +6,11 @@ import matplotlib
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
-exampleDataset = datasets.FashionMNIST(root="data", train=True, download=True, transform=torchvision.transforms.ToTensor())
+example_dataset: torch.Tensor = datasets.FashionMNIST(root="data", train=True, download=True, transform=torchvision.transforms.ToTensor())
+figure = plt.figure(figsize=(8, 8))
+rows, cols = 3, 3
+print(torch.randint(len(example_dataset), size=(1, 0)))
+for i in range(1, rows * cols + 1):
+    sample_index = torch.randint(len(example_dataset), size=(1, )).item()
+    img, label = example_dataset[sample_index]
+    print(example_dataset[sample_index])
