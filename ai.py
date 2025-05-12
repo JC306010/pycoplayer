5d21ecc6fd5a84ae14b69cc20fd8e5521d5d1e97
310464 <156846632+310464@users.noreply.github.com>
2025-05-12 13:08:23 -0500

    updates

--- ai.py
+++ ai.py
@@ -2,15 +2,14 @@ import screen
 import cv2
 
 screens = screen.AIScreenshot()
-ocr = screen.CharacterRecognition()
 videoCapture = cv2.VideoCapture(0)
 
 while True:
-    isFrame, frame = videoCapture.read()
+    screens.take_screenshot()
     videoCapture.set(cv2.CAP_PROP_FPS, 30)
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    cv2.imshow("screen", screens.array)
     
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+    if (cv2.waitKey(25) & 0xFF) == ord('q'):
         break
     
 videoCapture.release()
