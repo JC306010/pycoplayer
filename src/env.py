3ce2c32bb448eb7a15bf5819c9656637c2a29897
310464 <156846632+310464@users.noreply.github.com>
2025-05-21 13:08:48 -0500

    psda

--- src/env.py
+++ src/env.py
@@ -11,7 +11,6 @@ observation, info = env.reset()
 while True:
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
-    print(observation, "\n", reward, "\n", terminated, "\n", truncated, "\n", info)
     
     if terminated or truncated:
         observation, info = env.reset()
@@ -19,4 +18,5 @@ while True:
     if keyboard.is_pressed("q"):
         break
     
+print(observation, "\n", reward, "\n", terminated, "\n", truncated, "\n", info)
 env.close()
